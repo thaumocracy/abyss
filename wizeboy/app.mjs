c529df6624106bb35adf76d8ee1d1585d06b9c29
thaumocracy <thunpwnz@gmail.com>
2022-06-26 19:49:09 +0300

    dump

--- wizeboy/app.mjs
+++ wizeboy/app.mjs
@@ -13,8 +13,49 @@ const testQuote = {
   text: "“A room without books is like a body without a soul.”",
 };
 
+const pictures = [
+  {
+    title: "Звездная ночь",
+    author: "Винсент Ван Гог",
+    country: "🇳🇱",
+    year: "1889",
+    link: "https://losko.ru/wp-content/uploads/2019/11/7702b9faef4382f33090b0b87b2c0ad0-1.jpg",
+  },
+  {
+    title: "Черный квадрат",
+    author: "Казимир Малевич",
+    country: "🇺🇦",
+    year: "1915",
+    link: "https://losko.ru/wp-content/uploads/2019/11/CHe-rnyi-_suprematicheskii-_kvadrat._1915._GTG.png",
+  },
+  {
+    title: "Девочка с персиками",
+    author: "Валентин Александрович Серов",
+    country: "🇷🇺",
+    year: "1887",
+    link: "https://losko.ru/wp-content/uploads/2019/11/Valentin_Serov_-_Devochka_s_persikami._Portret_V.S.Mamontovoi-_-_Google_Art_Project.jpg",
+  },
+  {
+    title: "Девушка с жемчужной сережкой",
+    author: "Ян Вермеер",
+    country: "🇳🇱",
+    year: "1665",
+    link: "https://losko.ru/wp-content/uploads/2019/11/cover-1.jpg",
+  },
+  {
+    title: "Постоянство памяти",
+    author: "Сальвадор Дали",
+    country: "🇪🇸",
+    style: "",
+    type: "",
+    year: "1889",
+    link: "https://losko.ru/wp-content/uploads/2019/11/ebd7ff7f9d8ddb7b9a8502168187.jpg",
+  },
+];
+
 client.on("message", (msg) => {
-  const channel = client.channels.cache.get(`514836808312553472`);
+  const channel = client.channels.cache.get(msg.channel.id);
+  console.log(msg.channel.id);
   // console.log(channel);
   switch (msg.content) {
     case "ping":
@@ -23,9 +64,13 @@ client.on("message", (msg) => {
     case "::q":
       channel.send(`*${testQuote.text}*\n**${testQuote.author}**`);
       break;
-    case "sendpic":
+    case "::pic":
+      const picture = pictures[Math.floor(Math.random() * pictures.length)];
+      channel.send(
+        `"${picture.title}" by ${picture.author}. ${picture.country}, ${picture.year}.`
+      );
       channel.send({
-        files: ["https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg"],
+        files: [picture.link],
       });
       break;
     default:
