997eed5c768da8e07cf1221cbdb4823e072aab2c
thaumocracy <thunpwnz@gmail.com>
2022-07-27 20:33:54 +0300

    weather rebort

--- wizeboy/app.mjs
+++ wizeboy/app.mjs
@@ -3,6 +3,7 @@ import keys from "./keys.mjs";
 import art from "./data/art.mjs";
 import verses from "./data/verses.mjs";
 import quotes from "./data/quotes.mjs";
+import fetch from "node-fetch";
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -15,11 +16,41 @@ client.on("message", (msg) => {
   const channel = client.channels.cache.get(msg.channel.id);
   console.log(msg.channel.id);
   // console.log(channel);
-  console.log(msg.content);
-  switch (msg.content) {
+  console.log(msg.content.split(" "));
+  const input = msg.content.split(" ");
+  switch (input[0]) {
     case "ping":
       channel.send(`PREVED BUHANKA`);
       break;
+    case "::w":
+      if (input[1]) {
+        fetch(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${input[1]}&limit=1&appid=${keys.openweather}`
+        )
+          .then((data) => data.json())
+          .then((resp) => {
+            fetch(
+              `https://api.weather.yandex.ru/v2/informers?lat=${resp[0].lat}&lon=${resp[0].lon}`,
+              {
+                headers: {
+                  "X-Yandex-API-Key": keys.yandex_weather,
+                },
+              }
+            )
+              .then((data) => data.json())
+              .then((response) => {
+                console.log(response.fact.temp);
+                channel.send(
+                  `Сейчас в городе ${input[1]} ${response.fact.temp} градусов`
+                );
+              });
+          });
+        break;
+      } else {
+        channel.send(`Укажите город`);
+        break;
+      }
+
     case "::q":
     case "::quote":
     case "::цитата":
